20d03b8a98e4e9a40acbd50f3352a440a4fb6fa9
kenneyv7143 <kenneyv7143@student.faytechcc.edu>
2026-02-12 14:23:26 -0500

    Add files via upload

---
+++ p1lab1_kenneyv.py
@@ -0,0 +1,9 @@
+# valerie kenney
+# Dat02/12/2026
+# p1lab1
+# Python program that will display a user's name
+# write the code to get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ") 
+# display output
+print("Hello,",first_name, last_name + "! Welcome to CTI-110")
