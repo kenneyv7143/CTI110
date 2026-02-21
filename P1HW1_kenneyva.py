bed13014f08a452917db0ed891ef8f5271c4f1cb
kenneyv7143 <kenneyv7143@student.faytechcc.edu>
2026-02-20 22:03:16 -0500

    Add files via upload

---
+++ P1HW1_kenneyva.py
@@ -0,0 +1,24 @@
+# Valeriekenney
+ # Date2/20/26
+ # P1HW1
+ # PYTHON pgm using mathematical expressions
+print("-------Calculating Exponents--------")
+print()
+#identifying base and exponent to come find solution
+print()
+a=int(input("Enter an integer as the base value:"))
+print()
+b=int(input("Enter an integer as the exponent:")) 
+c=a**b
+print(a,"raised to the power of",b,"=",c)
+print()
+print("-------Addittion and Subtraction--------")
+print()
+a=int(input("Enter a starting integer :"))
+print()
+b=int(input("Enter an integer to add:")) 
+print()
+c=int(input("Enter an integer to subtract:")) 
+d=a+b-c
+print()
+print(a,"+",b,"-",c,"is equal to",d)
