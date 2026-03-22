63557ad4d54dae70e1522ed6bb08068da9db23fa
kenneyv7143 <kenneyv7143@student.faytechcc.edu>
2026-03-21 20:25:22 -0400

    Add files via upload

---
+++ P3LAB_Kenneyv.py
@@ -0,0 +1,56 @@
+# KenneyV
+# 3/19/2026
+# P3LAB
+# COIN CALCULATION
+
+change = float (input("Enter an amount of money: $"))
+
+print (f"Change Amount:{change}")
+
+#converting the value to an integer
+change = round (change * 100)
+
+#Determine how many dollars are needed
+num_dollars = change // 100
+change = change - (num_dollars * 100)
+
+num_quarters = change // 25
+change = change - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change
+
+if num_dollars > 0:
+    if num_dollars == 1:
+        print(f"{num_dollars} Dollar")
+    else :
+        print (f"{num_dollars} Dollars")    
+
+if num_quarters > 0:
+    if num_quarters == 1:
+        print(f"{num_quarters} quarter")
+    else :
+        print (f"{num_quarters} quarters")       
+
+if num_dimes > 0:
+    if num_dimes == 1:
+        print(f"{num_dimes} Dime")
+    else :
+        print (f"{num_dimes} Dimes")           
+
+if num_nickels > 0:
+    if num_nickels == 1:
+        print(f"{num_nickels} Nickel")
+    else :
+        print (f"{num_nickels} Nickels") 
+
+if num_pennies > 0:
+    if num_pennies == 1:
+        print(f"{num_pennies} Penney")
+    else :
+        print (f"{num_pennies} Pennies")        
